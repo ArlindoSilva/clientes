b69ae915810943cbf3f5f61fadfb8279a51bc3a2
ArlindoSilva <arlindoas2@gmail.com>
2025-01-16 14:39:49 -0300

    Injetando a camada de repository na camada de resources e utilizando-o no método listar

--- src/main/java/com/arlindo/clientes/resources/ClientesResources.java
+++ src/main/java/com/arlindo/clientes/resources/ClientesResources.java
@@ -1,24 +1,23 @@
 package com.arlindo.clientes.resources;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arlindo.clientes.domain.Cliente;
+import com.arlindo.clientes.repository.ClientesRepository;
 
 @RestController
 public class ClientesResources {
+	
+	@Autowired
+	private ClientesRepository clientesRepository;
 
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		
-		Cliente c1 = new Cliente("Ana");
-		Cliente c2 = new Cliente("João");
-		
-		Cliente[] clientela = {c1, c2};
-		
-		return Arrays.asList(clientela);
-	}
+		return clientesRepository.findAll();
+	} 
 }
